f047f1df404a53618d0864e5af1ad7b8f461d091
muridot0 <muri0@outlook.com>
2023-07-02 22:03:06 +0100

    added options for user and space

--- commands/addtally.js
+++ commands/addtally.js
@@ -2,9 +2,26 @@ const { SlashCommandBuilder } = require('discord.js')
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName('add_tally')
-    .setDescription('adds a tally for a specific user'),
+    .setName('add_tally_to')
+    .setDescription('adds a tally for a specific user')
+    .addUserOption((option) =>
+      option
+        .setName('target')
+        .setDescription('The member to add a tally to')
+        .setRequired(true)
+    )
+    .addStringOption((option) =>
+      option
+        .setName('space')
+        .setDescription('The space to add the tally to')
+        .setRequired(true)
+        .setMaxLength(100)
+    ),
   async execute(interaction) {
-    await interaction.reply(`${interaction.user.username} added 1 tally`)
+    const user = interaction.options.getUser('target')
+    const space = interaction.options.getString('space')
+    await interaction.reply(
+      `${interaction.user.username} added 1 tally to ${user} in ${space}`
+    )
   }
 }
